97199a93d2e301de9b081af883bf0d6c9ea2875d
Boris Abiduev <abidyev@gmail.com>
2022-02-21 02:03:44 +0800

    change

--- src/main/java/app/entities/TableOperations.java
+++ src/main/java/app/entities/TableOperations.java
@@ -2,6 +2,7 @@ package app.entities;
 
 import javax.swing.*;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class TableOperations {
 
@@ -26,25 +27,43 @@ public class TableOperations {
 
     public static void columnName() throws SQLException {
         Connection con = DriverManager.getConnection(url, user, password);
-        PreparedStatement ps = con.prepareStatement("SELECT From list_arrays");
+        PreparedStatement ps = con.prepareStatement("select * from list_arrays");
         ResultSet rs = ps.executeQuery();
         ResultSetMetaData meta = rs.getMetaData();
-        String colname1 = meta.getColumnName(1);
 
-        System.out.println(colname1);
+        int columnCount = meta.getColumnCount();
 
+        ArrayList<String> nameColumn = new ArrayList<>();
+        for (int i = 1; i < columnCount; i++) {
+            String columnName = meta.getColumnName(i);
+            nameColumn.add(columnName);
+        }
+        System.out.println(nameColumn);
+
+        String query = "";
+
+        for(int i = 1; i < nameColumn.size(); i++){
+            query = query + nameColumn.get(i) + ", ";
+        }
+
+        StringBuilder sb = new StringBuilder(query);
+        sb.deleteCharAt(sb.length()-2);
+        query = sb.toString();
+        String e = query.trim();
+
+        System.out.println(e);
     }
 
     public static void alterTable() throws SQLException {
-        if (Calculation.nowColumns != columns){
+        if (Calculation.nowColumns != columns) {
             int create = Calculation.nowColumns - columns + 1;
             System.out.println("created columns = " + create);
-            Integer [] clmns = new Integer[create];
+            Integer[] clmns = new Integer[create];
 
             for (int i = 0; i < clmns.length; i++) {
                 columns++;
                 clmns[i] = columns;
-                String sql="ALTER TABLE list_arrays ADD column column_"+clmns[i]+" varchar(10)";
+                String sql = "ALTER TABLE list_arrays ADD column column_" + clmns[i] + " varchar(10)";
                 //String sql="ALTER TABLE table_name ADD column column_"+clmns[i]+" varchar(10)";
                 Connection con = DriverManager.getConnection(url, user, password);
                 PreparedStatement ps = con.prepareStatement(sql);
